e7a857299814ccc2ed9c20e18bbcf164fe1edd6f
jn <jn@kali.kali>
2022-05-08 13:45:31 -0400

    Added cobra to generate help command and added the version command.

--- main.go
+++ main.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 )
+
+const appName = "flyawayhub"
+const version = "1.0.0"
+
+var versionCmd = &cobra.Command{
+	Use: "version",
+	Short: "Print the version number of " + appName,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(appName + " version " + version)
+	},
+}
+
+func init() {
+	var rootCmd = &cobra.Command{Use: "flyawayhub-cli"}
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.Execute()
+}
 
 func Hello() string {
 	return "Hello World!"
