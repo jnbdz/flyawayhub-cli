53c4386cc71d4db94bf0a543d02de66a81b92c9e
jnbdz <jean-nicolas.boulaydesjardins.ext@montreal.ca>
2024-02-21 20:15:14 -0500

    Clean up a lot related to authentication.

--- auth/service.go
+++ auth/service.go
@@ -6,22 +6,73 @@ package auth
 
 import (
 	"context"
-	"flyawayhub-cli/logging" // Assume logging is your logging package
+	appConfig "flyawayhub-cli/config"
+	"flyawayhub-cli/logging"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
 )
 
+// Service interface defines the methods required for authentication.
 type Service interface {
-	Login(ctx context.Context, username, password string) error
+	Login(
+		ctx context.Context,
+		username,
+		password string,
+		triggerFetchOrganizationInfo func(accessToken string) error) error
 }
 
+// authService implements the Service interface.
 type authService struct {
 	logger logging.Logger
 }
 
+// NewAuthService returns a new instance of an authService.
 func NewAuthService(logger logging.Logger) Service {
 	return &authService{logger: logger}
 }
 
-func (s *authService) Login(ctx context.Context, username, password string) error {
-	// Implement login logic, using s.logger for logging
+// Login handles the user login process.
+func (s *authService) Login(
+	ctx context.Context,
+	username,
+	password string,
+	triggerFetchOrganizationInfo func(accessToken string) error) error {
+	// Load AWS configuration
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(appConfig.AWSCognitoRegion))
+	if err != nil {
+		s.logger.Error("Unable to load SDK config", "error", err)
+		return err
+	}
+
+	// Create a Cognito Identity Provider client
+	svc := cognitoidentityprovider.NewFromConfig(cfg)
+
+	// Replace these values with your Cognito app's details
+	clientId := appConfig.AWSCognitoClientID
+
+	// Perform the authentication request
+	authResp, err := svc.InitiateAuth(context.TODO(), &cognitoidentityprovider.InitiateAuthInput{
+		AuthFlow: types.AuthFlowTypeUserPasswordAuth, // Use the enum for the auth flow type
+		AuthParameters: map[string]string{
+			"USERNAME": username,
+			"PASSWORD": password,
+		},
+		ClientId: aws.String(clientId),
+	})
+	if err != nil {
+		s.logger.Error("authentication failed: %v\n", err)
+		return err
+	}
+
+	// Fetch and update session with organization info
+	// FetchOrganizationInfo(*authResp.AuthenticationResult.AccessToken, "json")
+	if err := triggerFetchOrganizationInfo(*authResp.AuthenticationResult.AccessToken); err != nil {
+		s.logger.Error("Failed to fetch organization info: %v\n", err)
+		// Decide how you want to handle this error. For now, just printing the error.
+	}
+
+	s.logger.Info("Login successful")
 	return nil
 }
--- cmd/auth.go
+++ cmd/auth.go
@@ -7,44 +7,12 @@ import (
 	appConfig "flyawayhub-cli/config"
 	"flyawayhub-cli/logging"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
 	"os"
 	"strings"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 	"github.com/spf13/cobra"
 	"golang.org/x/crypto/ssh/terminal"
 )
-
-var (
-	logger logging.Logger
-)
-
-type authService struct{}
-
-// Login
-func (s *authService) Login(ctx context.Context) error {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Username: ")
-	username, _ := reader.ReadString('\n')
-	username = strings.TrimSpace(username)
-
-	fmt.Print("Enter Password: ")
-	bytePassword, _ := terminal.ReadPassword(0)
-	password := string(bytePassword)
-
-	return sendCredentials(ctx, username, password)
-}
-
-func loginProcess(ctx context.Context, username, password string) error {
-	err := sendCredentials(ctx, username, password)
-	if err != nil {
-		return err
-	}
-	return nil
-}
 
 // NewLoginCommand updated to use loginProcess within the Run function.
 func NewLoginCommand(authService auth.Service, logger logging.Logger) *cobra.Command {
@@ -54,27 +22,28 @@ func NewLoginCommand(authService auth.Service, logger logging.Logger) *cobra.Com
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.Background() // Or context.TODO() if unsure
 
-			reader := bufio.NewReader(os.Stdin)
 			fmt.Print("Enter Username: ")
-			username, err := reader.ReadString('\n')
+			username, err := bufio.NewReader(os.Stdin).ReadString('\n')
 			if err != nil {
-				logger.Error("Error reading username", "error", err)
+				logger.Error("Error reading username", map[string]interface{}{"error": err})
 				return err
 			}
 			username = strings.TrimSpace(username)
 
 			fmt.Print("Enter Password: ")
 			bytePassword, err := terminal.ReadPassword(0)
+			fmt.Println() // Move to a new line after password input.
 			if err != nil {
-				logger.Error("Error reading password", "error", err)
+				logger.Error("Error reading password", map[string]interface{}{"error": err})
 				return err
 			}
 			password := string(bytePassword)
 
 			// Use authService to perform login
-			err = authService.Login(ctx, username, password)
-			if err != nil {
-				logger.Error("Login process failed", "error", err)
+			if err := authService.Login(ctx, username, password, func(accessToken string) error {
+				return FetchOrganizationInfo(accessToken, "json")
+			}); err != nil {
+				logger.Error("Login failed", map[string]interface{}{"error": err})
 				return err
 			}
 
@@ -85,74 +54,10 @@ func NewLoginCommand(authService auth.Service, logger logging.Logger) *cobra.Com
 	return cmd
 }
 
-// sendCredentials
-func sendCredentials(ctx context.Context, username, password string) error {
-	// Load AWS configuration
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(appConfig.AWSCognitoRegion))
-	if err != nil {
-		logger.Error("Unable to load SDK config", "error", err)
-		return err
-	}
+func InitCommands(root *cobra.Command, authService auth.Service, logger logging.Logger) {
+	// Instantiate the login command with required dependencies
+	loginCmd := NewLoginCommand(authService, logger)
 
-	// Create a Cognito Identity Provider client
-	svc := cognitoidentityprovider.NewFromConfig(cfg)
-
-	// Replace these values with your Cognito app's details
-	clientId := appConfig.AWSCognitoClientID
-
-	// Perform the authentication request
-	authResp, err := svc.InitiateAuth(context.TODO(), &cognitoidentityprovider.InitiateAuthInput{
-		AuthFlow: types.AuthFlowTypeUserPasswordAuth, // Use the enum for the auth flow type
-		AuthParameters: map[string]string{
-			"USERNAME": username,
-			"PASSWORD": password,
-		},
-		ClientId: aws.String(clientId),
-	})
-	if err != nil {
-		logger.Error("authentication failed: %v\n", err)
-		return err
-	}
-
-	// Fetch and update session with organization info
-	if err := FetchOrganizationInfo(*authResp.AuthenticationResult.AccessToken, "json"); err != nil {
-		logger.Error("Failed to fetch organization info: %v\n", err)
-		// Decide how you want to handle this error. For now, just printing the error.
-	}
-
-	logger.Info("Login successful")
-	return nil
-}
-
-var loginCmd = &cobra.Command{
-	Use:   "login",
-	Short: "Login will sign you in to " + appConfig.AppName + " (will generate an authorization bearer).",
-	Run:   login,
-}
-
-func login(cmd *cobra.Command, args []string) {
-	ctx := context.Background() // Or context.TODO() if unsure
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter Username: ")
-	username, _ := reader.ReadString('\n')
-	username = strings.TrimSpace(username)
-
-	fmt.Print("Enter Password: ")
-	bytePassword, err := terminal.ReadPassword(0)
-	if err != nil {
-		logger.Error("\nError reading password")
-		return
-	}
-	password := string(bytePassword)
-
-	// Send credentials using the AWS SDK
-	err = sendCredentials(ctx, username, password)
-	if err != nil {
-		return
-	}
-}
-
-func InitCommands(root *cobra.Command) {
+	// Add the login command to the root command
 	root.AddCommand(loginCmd)
-	// Add other commands as needed
 }
